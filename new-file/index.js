c4372f0b6c6299ffd11eee7631d003393a80cde5
amanojha965 <amanojha9650@gmail.com>
2026-02-20 15:41:27 +0530

    [Aman] delete the new file due to duplicacy

--- new-file/index.js
+++
@@ -1,5 +0,0 @@
-const {capitalString,reverseString,countVowels}=require('./stringUtils.js');
-const str="Aman ojha";
-console.log(capitalString(str));
-console.log(reverseString(str));
-console.log(countVowels(str));
